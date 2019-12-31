fc2078ab7d12c16a2d65b8076e9c0099a63abeda
kdeveloper87 <k.developer87@gmail.com>
2019-12-31 15:23:24 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry:  './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src/js')
+        ],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  devtool: 'source-map',
+  // https://webpack.js.org/concepts/mode/#mode-development
+  mode: 'development'
+};
