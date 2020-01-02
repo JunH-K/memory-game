8d76263c0538c6faa5622382d18dd89af2dfecca
jun <nr0307@amoeba.co.kr>
2020-01-02 13:09:07 +0900

    [refactoring]클래스 변환

---
+++ src/js/Main.js
@@ -0,0 +1,215 @@
+import datas from "./datas/Datas";
+
+export default class Main {
+  static REMAIN_TIME = 60;
+  remainTime = Main.REMAIN_TIME;
+  clickElement = [];
+  matchingCount = 0;
+  interval = null;
+  isRestart = false;
+  containerElem;
+  dimElem;
+  startWrap;
+  startElem;
+  timeElem;
+
+  init() {
+    this.containerElem = document.querySelector( '.container' );
+    this.dimElem = document.querySelector( '.dim' );
+    this.startWrap = document.querySelector( '.start_wrap' );
+    this.startElem = document.querySelector( '.start' );
+    this.timeElem = document.querySelector( '#time-remaining' );
+    this.containerElem.addEventListener( 'click', this.onClickCard );
+    this.startElem.addEventListener( 'click', this.startGame );
+    this.initCard();
+  }
+
+  initCard() {
+    const dataSource = this.shuffle( datas );
+    // const container = document.querySelector( '.container' );
+    const cards = document.createDocumentFragment();
+
+    dataSource && dataSource.forEach( (item) => {
+      const { frontImage, backImage, key } = item;
+      cards.append( this.createCard( { frontImage, backImage, key } ) );
+    } );
+
+    this.containerElem.appendChild( cards );
+
+    if( !this.getStorage( 'scores' ) ) {
+      this.setStorage( 'scores', [] )
+    } else {
+      this.updateScore();
+    }
+  }
+
+  startGame=(e)=> {
+    if( !this.isRestart && e.target.classList.contains( 'start_wrap' ) ) {
+      this.startInterval();
+    } else {
+      this.reset();
+      this.startInterval();
+    }
+
+    this.hide( this.dimElem );
+    this.hide( this.startWrap );
+
+  }
+
+  setStorage(key, obj) {
+    const myStorage = window.localStorage;
+    return myStorage.setItem( key, JSON.stringify( obj ) );
+  }
+
+  getStorage(key) {
+    const myStorage = window.localStorage;
+    return JSON.parse( myStorage.getItem( key ) );
+  }
+
+  updateScore(score) {
+    const scoreElem = document.querySelector( '.score' );
+    const frag = document.createDocumentFragment();
+    const scores = this.getStorage( 'scores' );
+    score && scores.push( score );
+    scores.sort();
+
+    Array.from( scoreElem.childNodes ).forEach( (child) => {
+      scoreElem.removeChild( child );
+    } );
+
+    scores.forEach( (item, index) => {
+      const liElem = document.createElement( 'li' );
+      if( index > 2 ) {
+        return;
+      }
+      this.setText( liElem, `${ index + 1 }위 ${ item }초` );
+      frag.appendChild( liElem );
+    } );
+
+    scoreElem.appendChild( frag );
+    this.setStorage( 'scores', scores );
+  }
+
+  onClickCard=(e)=> {
+    if( this.clickElement.length < 2 && e.target.classList.contains( 'card-side-back' ) ) {
+      e.target.parentNode.classList.add( 'flip' );
+      this.clickElement.push( e.target );
+      this.checkMatching();
+    }
+  }
+
+  checkMatching = this.debounce( () => {
+    if( this.clickElement.length !== 2 ) {
+      return;
+    }
+
+    if( this.clickElement[ 0 ].dataset.key === this.clickElement[ 1 ].dataset.key ) {
+      this.matchingCount++;
+    } else {
+      this.clickElement.forEach( (i) => {
+        i.parentNode.classList.remove( 'flip' );
+      } );
+    }
+
+    this.checkWin( this.matchingCount );
+    this.clickElement = [];
+  }, 500 );
+
+  checkWin(count) {
+    if( count === 8 ) {
+      clearInterval( this.interval );
+      this.show( this.dimElem );
+      this.startWrap.style.display = 'flex';
+      this.setText( this.startElem, "Complete! <br/> Restart" );
+      this.isRestart = true;
+      this.updateScore( Main.REMAIN_TIME - this.remainTime );
+    }
+  }
+
+  createCard({ frontImage, backImage, key }) {
+    const cardDiv = document.createElement( 'div' );
+    const frontDiv = document.createElement( 'div' );
+    const backDiv = document.createElement( 'div' );
+
+    cardDiv.classList.add( 'card' );
+    frontDiv.classList.add( 'card-side', 'card-side-front' );
+    backDiv.classList.add( 'card-side', 'card-side-back' );
+
+    frontDiv.style.backgroundImage = `url("${ frontImage }")`;
+    frontDiv.style.backgroundRepeat = 'no-repeat';
+    backDiv.dataset.key = key;
+
+    cardDiv.appendChild( frontDiv );
+    cardDiv.appendChild( backDiv );
+
+    return cardDiv;
+  }
+
+  reset() {
+    //남은시간초기화
+    this.setText( this.timeElem, Main.REMAIN_TIME );
+    this.matchingCount = 0;
+    this.remainTime = Main.REMAIN_TIME;
+
+    //카드 초기화
+    Array.from( this.containerElem.childNodes ).forEach( (child) => {
+      if( child.classList && child.classList.contains( 'card' ) ) {
+        this.containerElem.removeChild( child );
+      }
+    } );
+    //initCard
+    this.initCard();
+  }
+
+  shuffle(datas) {
+    const double = [ ...datas, ...datas.reverse() ];
+    const { length } = double;
+
+    for ( let i = 0; i < length; i++ ) {
+      const random = Math.floor( Math.random() * length );
+      [ double[ i ], double[ random ] ] = [ double[ random ], double[ i ] ];
+    }
+
+    return double;
+  }
+
+  debounce(func, delay) {
+    let timer = null;
+
+    return function () {
+      if( timer ) {
+        clearTimeout( timer );
+      }
+      timer = setTimeout( func, delay );
+    }
+  }
+
+  show(elem) {
+    elem.style.display = 'block';
+  }
+
+  hide(elem) {
+    elem.style.display = 'none';
+  }
+
+  setText(elem, text) {
+    elem.innerHTML = text;
+  }
+
+  startInterval=()=> {
+    this.interval = setInterval( () => {
+      this.remainTime -= 1;
+      this.setText( this.timeElem, this.remainTime );
+
+      if( this.remainTime === 0 ) {
+        clearInterval( this.interval );
+        this.show( this.dimElem );
+        this.startWrap.style.display = 'flex';
+        this.setText( this.startElem, "Game Over<br/>Restart" );
+        this.isRestart = true;
+        this.updateScore( Main.REMAIN_TIME - this.remainTime );
+      }
+    }, 1000 );
+  }
+
+}
---
+++ src/js/datas/Datas.js
@@ -0,0 +1,21 @@
+import angular from "../../images/angular.png";
+import node from "../../images/node.svg";
+import react from "../../images/react.png";
+import vue from "../../images/vue.png";
+import html from "../../images/html.png";
+import github from "../../images/github.png";
+import ws from "../../images/ws.png";
+import docker from "../../images/docker.png";
+
+const datas = [
+  { frontImage: angular, backImage: 'js.png', key: 1 },
+  { frontImage: node, backImage: 'js.png', key: 2 },
+  { frontImage: react, backImage: 'js.png', key: 3 },
+  { frontImage: vue, backImage: 'js.png', key: 4 },
+  { frontImage: html, backImage: 'js.png', key: 5 },
+  { frontImage: github, backImage: 'js.png', key: 6 },
+  { frontImage: ws, backImage: 'js.png', key: 7 },
+  { frontImage: docker, backImage: 'js.png', key: 8 },
+];
+
+export default datas;
